d8bcbb8e2d356001d26cb00554355ef19294a0f3
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-06-12 20:07:40 +0300

    js-цикл for/php-сигналы

--- test.php
+++ test.php
@@ -1,44 +1,48 @@
 <?php
 
-function getMensCountByYear($users) 
+const FREE_EMAIL_DOMAINS = [
+    'gmail.com', 'yandex.ru', 'hotmail.com'
+];
+
+function getFreeDomainsCount($emails) 
 {
-    // фильтруем массив и отбираем мужчин(male)
-    $users = array_filter($users, function($user) {
-        return $user['gender'] == 'male';
-    });
-    // date(Y, strtotime()) - извлекаем год рождения (1973, 1963 ....)
-    // array_key_exists - проверяем присутствие элемента в массиве
-    // считаем количество мужчин по годам
-    $bigUserNum = array_reduce($users, function ($acc, $user) {
-        if (array_key_exists((date("Y", strtotime($user['birthday']))), $acc)) {
-            $acc[(date("Y", strtotime($user['birthday'])))] += 1;
-        }else {
-            $acc[(date("Y", strtotime($user['birthday'])))] = 1; 
+    //отделяем домен
+    $domains = array_map(function ($email) {
+        $domainArr = explode( '@',  $email);
+        return $domainArr[1];
+    }, $emails);
+    // проверяем присутствие домена в массиве доменов 'FREE_EMAIL_DOMAINS' - in_array
+    // создаем новый массив с подходящими доменами и считаем их количество
+    // array_key_exists - проверяет наличие ключа в массиве (так-как домен в новом массиве является ключом)
+    $freeDomainCount = array_reduce($domains, function($acc, $domain){
+        if (in_array($domain, FREE_EMAIL_DOMAINS)){
+            if (array_key_exists($domain, $acc)){
+            $acc[$domain] += 1;
+            }else{
+                $acc[$domain] = 1;
+            }
         }
         return $acc;
     }, []);
-    return $bigUserNum;
+    
+    return $freeDomainCount;
 }
 
- $users = [
-    ['name' => 'Bronn', 'gender' => 'male', 'birthday' => '1973-03-23'],
-    ['name' => 'Reigar', 'gender' => 'male', 'birthday' => '1973-11-03'],
-    ['name' => 'Eiegon',  'gender' => 'male', 'birthday' => '1963-11-03'],
-    ['name' => 'Sansa', 'gender' => 'female', 'birthday' => '2012-11-03'],
-    ['name' => 'Jon', 'gender' => 'male', 'birthday' => '1980-11-03'],
-    ['name' => 'Robb','gender' => 'male', 'birthday' => '1980-05-14'],
-    ['name' => 'Tisha', 'gender' => 'female', 'birthday' => '2012-11-03'],
-    ['name' => 'Rick', 'gender' => 'male', 'birthday' => '2012-11-03'],
-    ['name' => 'Joffrey', 'gender' => 'male', 'birthday' => '1999-11-03'],
-    ['name' => 'Edd', 'gender' => 'male', 'birthday' => '1973-11-03']
+$emails = [
+    'info@gmail.com',
+    'info@yandex.ru',
+    'info@hotmail.com',
+    'mk@host.com',
+    'support@hexlet.io',
+    'key@yandex.ru',
+    'sergey@gmail.com',
+    'vovan@gmail.com',
+    'vovan@hotmail.com'
 ];
 
-print_r(getMensCountByYear($users));
-
+print_r(getFreeDomainsCount($emails));
 # => Array (
-#     1973 => 3,
-#     1963 => 1,
-#     1980 => 2,
-#     2012 => 1,
-#     1999 => 1
-# );
+#     'gmail.com' => 3
+#     'yandex.ru' => 2
+#     'hotmail.com' => 2
+#  )
