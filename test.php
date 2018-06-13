8f4ed71ea55247ff969657d3f680b27e4763fe09
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-06-13 07:08:14 +0300

    php - пользователь с меньшим количеством друзей

--- test.php
+++ test.php
@@ -1,48 +1,41 @@
 <?php
 
-const FREE_EMAIL_DOMAINS = [
-    'gmail.com', 'yandex.ru', 'hotmail.com'
-];
+function getFirstMenWithLessFriends($users) {
+    // проверяем на пустоту
+   if (empty($users)){
+       return null;
+   }
+  
+   $minFriends = array_reduce($users, function ($acc, $user){
+       // проверяем на пустоту $acc и добавляем $user 
+       if (empty($acc)){
+           $acc = $user;
+       }
+       // находим первого пользователя с меньшим количеством друзей
+       if (count($user['friends']) < count($acc['friends'])){
+          $acc = $user;
+       }
+       return $acc;
+   }, []);
 
-function getFreeDomainsCount($emails) 
-{
-    //отделяем домен
-    $domains = array_map(function ($email) {
-        $domainArr = explode( '@',  $email);
-        return $domainArr[1];
-    }, $emails);
-    // проверяем присутствие домена в массиве доменов 'FREE_EMAIL_DOMAINS' - in_array
-    // создаем новый массив с подходящими доменами и считаем их количество
-    // array_key_exists - проверяет наличие ключа в массиве (так-как домен в новом массиве является ключом)
-    $freeDomainCount = array_reduce($domains, function($acc, $domain){
-        if (in_array($domain, FREE_EMAIL_DOMAINS)){
-            if (array_key_exists($domain, $acc)){
-            $acc[$domain] += 1;
-            }else{
-                $acc[$domain] = 1;
-            }
-        }
-        return $acc;
-    }, []);
-    
-    return $freeDomainCount;
+   return $minFriends;
 }
 
-$emails = [
-    'info@gmail.com',
-    'info@yandex.ru',
-    'info@hotmail.com',
-    'mk@host.com',
-    'support@hexlet.io',
-    'key@yandex.ru',
-    'sergey@gmail.com',
-    'vovan@gmail.com',
-    'vovan@hotmail.com'
+$users = [
+    ['name' => 'Tirion', 'friends' => [
+        ['name' => 'Mira', 'gender' => 'female'],
+        ['name' => 'Ramsey', 'gender' => 'male']
+    ]],
+    ['name' => 'Bronn', 'friends' => []],
+    ['name' => 'Sam', 'friends' => [
+        ['name' => 'Aria', 'gender' => 'female'],
+        ['name' => 'Keit', 'gender' => 'female']
+    ]],
+    ['name' => 'Keit', 'friends' => []],
+    ['name' => 'Rob', 'friends' => [
+        ['name' => 'Taywin', 'gender' => 'male']
+    ]],
 ];
 
-print_r(getFreeDomainsCount($emails));
-# => Array (
-#     'gmail.com' => 3
-#     'yandex.ru' => 2
-#     'hotmail.com' => 2
-#  )
+print_r(getFirstMenWithLessFriends($users));
+// => ['name' => 'Bronn', 'friends' => []];
