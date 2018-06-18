e47499a6d33101b74225e75a4fbe278cc650170b
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-06-18 21:54:43 +0300

    Expression and instructions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 
-export const getTrianglePerimeter = (a, b, c) => a + b + c;
+const square = ($num) => $num * $num;
+// возвращает сумму квадратов двух чисел
+const sumOfSquares = ($num, $num2) => square($num) + square($num2);
+// возвращает квадрат суммы квадратов двух чисел
+const squareSumOfSquares = ($num, $num2) => square(sumOfSquares($num, $num2));
 
-export function getTriangleArea(h, b) { 
-  return (1/2 * h * b);
-}
+console.log(square(5)); // 25)
+console.log(square(10)); // 100
+console.log(sumOfSquares(5, 10)); // 125
+console.log(sumOfSquares(10, -9)); // 181
+console.log(squareSumOfSquares(1, 1)); // 4
+console.log(squareSumOfSquares(2, 3)); // 169
