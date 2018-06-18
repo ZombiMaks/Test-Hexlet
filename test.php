58d6be8cd7b67aac4068d5bbb3b01303c7ef6d2a
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-06-18 18:49:23 +0300

    Парадигмы программирования

--- test.php
+++ test.php
@@ -1,20 +1,12 @@
 <?php
 
-function without(array $items, $value)
+function getDifference($array1, $array2)
 {
-    $value = func_get_args(); // Возвращает массив, содержащий аргументы функции
-    array_shift($value); // Извлекает первый элемент массива
-    $filtered = array_diff($items, $value); // Вычислить расхождение массивов
-    return array_values($filtered); // Сбрасываем ключи
+   $filterArray = array_filter($array1, function ($array) use ($array2) {
+        return !in_array($array, $array2);
+    });
+    return array_values($filterArray);
 }
 
-
-print_r(without([3, 4, 10, 4, 'true'], 4)); // => [3, 10, 'true']
-print_r(without(['3', 2], 0, 5, 11)); // => ['3', 2]
-//$this->assertEquals([], 
-print_r(without([], null));
-//$this->assertEquals([3, 10, 'true'],
-print_r(without([3, 4, 10, 4, 'true'], 4));
-print_r(without(['wow', 3, 4, 10, 4, 'true'], null, 4));
-print_r(without([3, 4, 10, 'true'], 3, 4));
-print_r(without(['3', 2], 0, 5));
+getDifference([2, 1], [2, 3]);
+// → [1]
