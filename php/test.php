1ce38e8c732b5e56eec2e7288332d5bdf9ff259b
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-06-19 22:45:56 +0300

    Одинаковая четность

--- php/test.php
+++ php/test.php
@@ -1,46 +1,21 @@
 <?php
 
-function decode($signal) 
+function getSameParity($array)
 {
-    // str_split умеет работать только с ASCII символами, поэтому для разделения строки на символы используйте конструкцию 
-   $str = preg_split("//u", $signal, -1, PREG_SPLIT_NO_EMPTY);
+    if (empty($array)){
+        return [];
+    }
 
-   // создаем пустую переменную в каторую будем записывать разшифрованный код
-   $result = '';
-   if (count($str) < 2){
-      return $result; 
-   }
-   $i = 0;
-   while ($i < count($str)) {
-        if ($str[$i] == '|') {
-            $result .= 1;
-            $i++;
-        }else{
-            $result .= 0;
+    $filter = [];
+    foreach ($array as $value){
+        // abs - модуль числа
+        if (abs($value % 2) == (abs($array[0])) % 2) {
+            $filter[] = $value; 
         }
-        $i++;
-   }
-   return $result;
+    }
+  return $filter;    
+
 }
 
-$signal = '_|¯|____|¯|__|¯¯¯';
-print_r(decode($signal)); // => '011000110100'
-
-//$signal_2 = '|¯|___|¯¯¯¯¯|___|¯|_|¯';
-//decode($signal_2); // => '110010000100111'
-
-//$signal_3 = '¯|___|¯¯¯¯¯|___|¯|_|¯';
-//decode($signal_3); // => '010010000100111'
-
-//$this->assertEquals('', 
-//decode('');
-//$this->assertEquals('', 
-//decode('|');
-//$this->assertEquals('010110010', 
-//decode('¯|__|¯|___|¯¯');
-//$this->assertEquals('010011000110', 
-//decode('_|¯¯¯|_|¯¯¯¯|¯¯');
-//$this->assertEquals('010010000100111', 
-//decode('¯|___|¯¯¯¯¯|___|¯|_|¯');
-//$this->assertEquals('110010000100111', 
-//decode('|¯|___|¯¯¯¯¯|___|¯|_|¯');
+print_r(getSameParity([2, 0, 1, -3, 10, -2])); // => []
+print_r(getSameParity([-1, 0, 1, -3, 10, -2])); // => [-1, 1, -3]
