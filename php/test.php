06cd2057424556177f2f9f0833e6e992bdf6301f
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-06-19 06:37:39 +0300

    increase array

--- php/test.php
+++ php/test.php
@@ -1,24 +1,29 @@
 <?php
 
+$arr = [
+    ['*','*','*','*'],
+    ['*',' ',' ','*'],
+    ['*',' ',' ','*'],
+    ['*','*','*','*']
+];
 
-function findIndexOfNearest(array $items, $value)
+function enlargeArrayImage($array)
 {
-    if (sizeof($items) === 0) {
-        return null;
+    $newArray = [];
+    $i = 0;
+    foreach ($array as $value){
+        foreach ($value as $item){
+            $newArray[$i][] = $item;
+            $newArray[$i][] = $item;
+        }
+        if ($i <= count($array) * 2){
+        $i++;
+        $newArray[$i] = $newArray[$i - 1];
+        $i++;
+        } 
     }
-
-    return array_reduce(array_keys($items), function ($acc, $i) use ($items, $value) {
-        return abs($items[$i] - $value) < abs($items[$acc] - $value) ? $i : $acc;
-    }, 0);
+    
+    return $newArray;
 }
 
-
-print_r(findIndexOfNearest([], 2)); // => null
-print_r(findIndexOfNearest([10], 0)); // => 2
-print_r(findIndexOfNearest([10, 15, 17, 19, 25, 30], 20));
-print_r(findIndexOfNearest([15, 10, 3, -5], -2));
-
-/*$this->assertNull(findIndexOfNearest([], 2));
-$this->assertEquals(0, findIndexOfNearest([10], 0));
-$this->assertEquals(1, findIndexOfNearest([10, 15], 20));
-$this->assertEquals(3, findIndexOfNearest([15, 10, 3, -5]-2));*/
+print_r(enlargeArrayImage($arr));
