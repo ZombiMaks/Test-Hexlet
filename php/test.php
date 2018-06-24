8c386e5a0c2dc5f076303394873045b0c4d53538
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-06-24 20:17:17 +0300

    Уровневое проектирование

--- php/test.php
+++ php/test.php
@@ -1,37 +1,124 @@
 <?php
 /*
-Реализуйте интерфейсные фунции точек:
-makeDecartPoint. Принимает на вход координаты и возвращает точку. Уже реализован.
-getX
-getY
-Получить x можно по формуле radius * cos(angle)
-Получить y можно по формуле radius * sin(angle)
+Реализуйте абстракцию (набор функций) для работы с прямоугольниками, стороны которого всегда параллельны осям. Прямоугольник может располагаться в любом месте координатной плоскости.
+
+При такой постановке, достаточно знать только три параметра для однозначного задания прямоугольника на плоскости: координаты левой-верхней точки, ширину и высоту. Зная их, мы всегда можем построить прямоугольник одним единственным способом.
+
+      |
+    4 |    точка   ширина
+      |       *-------------
+    3 |       |            |
+      |       |            | высота
+    2 |       |            |
+      |       --------------
+    1 |
+      |
+------|---------------------------
+    0 |  1   2   3   4   5   6   7
+      |
+      |
+      |
+Основной интерфейс:
+
+makeRectangle (конструктор) - создает прямоугольник. Принимает параметры: левую-верхнюю точку, ширину и высоту.
+Селекторы getStartPoint, getWidth и getHeight
+
+isContainsTheOrigin - проверяет, принадлежит ли центр координат прямоугольнику (не лежит на границе прямоугольника, а находится внутри). Чтобы в этом убедиться, достаточно проверить, что все точки прямоугольника лежат в разных квадрантах (их можно высчитать в момент проверки).
 */
 
+// Создание прямоугольника:
+// p - левая верхняя точка
+// 4 - ширина
+// 5 - высота
+//
+// p    4
+// -----------
+// |         |
+// |         | 5
+// |         |
+// -----------
+
+// получаем точки и выводим массив(коардинаты точки)
 function makeDecartPoint($x, $y)
 {
-     return [
-         'angle' => atan2($y, $x),
-         'radius' => sqrt($x ** 2 + $y ** 2)
-     ];
+    return [
+        'x' => $x,
+        'y' => $y
+    ];
 }
-
+// коардинат по Х
 function getX($point)
 {
-    return  $point['radius'] * cos($point['angle']);
+    return $point['x'];
 }
-
+// коардинат по У
 function getY($point)
 {
-  return  $point['radius'] * sin($point['angle']);
+    return $point['y'];
+}
+//проверяем в коких квадрантах лежат точки(вершины)
+function getQuadrant($point)
+{
+    $x = $point['x'];
+    $y = $point['y'];
+
+    if ($x > 0 && $y > 0) {
+        return 1;
+    } elseif ($x < 0 && $y > 0) {
+        return 2;
+    } elseif ($x < 0 && $y < 0) {
+        return 3;
+    } elseif ($x > 0 && $y < 0) {
+        return 4;
+    }
+
+    return null;
+};
+
+function makeRectangle($p, $width, $height)
+{
+    $leftTopPoint = getStartPoint($p);
+    $getWidth = getWidth($p, $width);
+    $getHeigth = getHeight($p, $height);
+    return [ $p, $getWidth, $getHeigth];
+} 
+
+function getStartPoint($p) 
+{
+    return $p;
 }
 
-$x = 4;
-$y = 8;
+function getHeight($p, $height){
 
-// $point хранит в себе данные в полярной системе координат
-$point = makeDecartPoint($x, $y);
+    return [
+        'x' => $p['x'], 
+        'y' => $p['y'] - $height
+    ];
+}
 
-// Здесь происходит преобразование из полярной в декартову
-print_r(getX($point)); // 4
-print_r(getY($point)); // 8
+function getWidth($p, $width)
+{
+    return [
+        'x' => $p['x'] + $width, 
+        'y' => $p['y']
+    ];
+}
+
+function isContainsTheOrigin($rectangle)
+{
+    $p1 = getQuadrant($rectangle[0]);
+    $p2= getQuadrant($rectangle[1]);
+    $p3 = getQuadrant($rectangle[2]);
+    if ($p1 === $p2 || $p1 === $p3 || $p2 === $p3){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+$p = makeDecartPoint(0, 1);
+$rectangle = makeRectangle($p, 4, 5);
+print_r(isContainsTheOrigin($rectangle)); // false
+
+$rectangle2 = makeRectangle(makeDecartPoint(-4, 3), 5, 4);
+print_r(isContainsTheOrigin($rectangle2)); // true
