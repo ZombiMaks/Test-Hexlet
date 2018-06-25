10f53736c4a5f6c26c5027b3821dc6504888a700
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-06-25 23:12:01 +0300

    Classes

--- php/test.php
+++ php/test.php
@@ -3,14 +3,32 @@
 Реализуйте класс Point с публичными свойствами $x и $y.
 Реализуйте функцию getMidpoint, которая принимает на вход две точки (объекты) и возвращает точку (объект) лежащую между ними (поиск середины отрезка).
 */
+class Point
+{
+  public $x;
+  public $y;
+}
+
+function getMidpoint($point1, $point2) 
+{
+  $x = ($point1->x + $point2->x) / 2;
+  $y = ($point1->y + $point2->y) / 2;
+  
+  $pointCenter = new Point();
+  $pointCenter->x = $x;
+  $pointCenter->y = $y;
+  
+  return $pointCenter;
+}
+
 
 $point1 = new Point();
-$point->x = 1;
-$point->y = 10;
+$point1->x = 1;
+$point1->y = 10;
 $point2 = new Point();
-$point->x = 10;
-$point->y = 1;
+$point2->x = 10;
+$point2->y = 1;
 
 $midpoint = getMidpoint($point1, $point2);
-$midpoint->x; // => 5.5
-$midpoint->y; // => 5.5
+print_r($midpoint->x); // => 5.5
+print_r($midpoint->y); // => 5.5
