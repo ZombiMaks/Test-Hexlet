9b37c15203f9f676711d69a7687c64f8f7ad2a47
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-06-26 22:17:36 +0300

    Указатели

--- php/test.php
+++ php/test.php
@@ -1,34 +1,27 @@
 <?php
 /*
-Реализуйте класс Point с публичными свойствами $x и $y.
-Реализуйте функцию getMidpoint, которая принимает на вход две точки (объекты) и возвращает точку (объект) лежащую между ними (поиск середины отрезка).
+Реализуйте функцию dup, которая клонирует переданную точку. Под клонированием подразумевается процесс создания нового объекта, с такими же данными как и у старого.
 */
+
 class Point
 {
-  public $x;
-  public $y;
+    public $x;
+    public $y;
 }
 
-function getMidpoint($point1, $point2) 
+function dup($point1)
 {
-  $x = ($point1->x + $point2->x) / 2;
-  $y = ($point1->y + $point2->y) / 2;
-  
-  $pointCenter = new Point();
-  $pointCenter->x = $x;
-  $pointCenter->y = $y;
-  
-  return $pointCenter;
+    $pointer = clone $point1;
+   return $pointer;        
 }
 
 
 $point1 = new Point();
-$point1->x = 1;
-$point1->y = 10;
-$point2 = new Point();
-$point2->x = 10;
-$point2->y = 1;
+$point1->x = 3;
+$point1->y = 5;
+$point2 = dup($point1);
+print_r($point2);
+print_r($point1);
 
-$midpoint = getMidpoint($point1, $point2);
-print_r($midpoint->x); // => 5.5
-print_r($midpoint->y); // => 5.5
+print_r($point1 == $point2); // true
+print_r($point1 === $point2); // false
