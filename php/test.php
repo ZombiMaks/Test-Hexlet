01d8fdea2ce04ed0b0d072f3323c95b5831f6c49
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-07-04 21:01:08 +0300

    Interface

--- php/test.php
+++ php/test.php
@@ -1,32 +1,33 @@
 <?php
 /*
-Реализуйте недостающие части класса Timer, который описывает собой таймер обратного отсчета. Необходимо дописать конструктор принимающий на вход три параметра: секунды, минуты (необязательный) и часы (необязательный). Конструктор должен подсчитать общее количество секунд для переданного времени и записать его в свойство secondsCount.
+Реализуйте класс User, который создает пользователей. Конструктор класса принимает на вход два параметра: идентификатор и имя.
 
-Воспользуйтесь константой SEC_PER_MIN для перевода минут в секунды (через умножение)
-Реализуйте дополнительную константу SEC_PER_HOUR и воспользуйтесь ей для перевода часов в секунды
+Реализуйте интерфейс Comparable для класса User. Сравнение пользователей происходит на основе их идентификатора.
 */
 
-class Time
+interface Comparable
 {
-    private $h;
-    private $m;
+    public function compareTo($user);
+}
+class User implements Comparable
+{
+    private $id;
+    private $name;
 
-    public static function fromString($time)
+    public function __construct($id, $name)
     {
-        return new self(intval(date("h" , strtotime($time))),  intval(date("i" , strtotime($time))));
+        $this->id = $id;
+        $this->name = $name;
     }
 
-    public function __construct($h, $m)
+    public function compareTo($user)
     {
-        $this->h = $h;
-        $this->m = $m;
+        return $this->id == $user->id;
     }
 
-    public function toString()
-    {
-        return "{$this->h}:{$this->m}";
-    }
 }
 
-$time = Time::fromString('3:8');
-print_r($time);
+$user1 = new User(4, 'tolya');
+$user2 = new User(4, 'petya');
+
+echo $user1->compareTo($user2); // false
