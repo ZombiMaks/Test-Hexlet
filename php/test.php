1f220a85bf62ac60ffddd8ca808e3a53aae4001f
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-07-05 23:08:52 +0300

    Random

--- php/test.php
+++ php/test.php
@@ -1,31 +1,55 @@
 <?php
 /*
-Реализуйте класс Circle для описания кругов. У круга есть только одно свойство - его радиус. Реализуйте методы getArea и getCircumference, которые возвращают площадь и периметр круга соответственно.
+Реализуйте генератор рандомных чисел представленный классом Random. Интерфейс объекта включает в себя три функции:
 
-Площадь круга: πr2
-Длина окружности: 2*πR
+Конструктор, принимает на вход seed, начальное число генератора псевдослучайных чисел.
+getNext - метод возврающающий новое случаное число
+reset - метод сбрасывающий генератор на начальное значение
+
+Простейший способ реализовать случайные числа это линейный конгруэнтный метод seed = (a * seed + c) % m; 
+$m = 2147483647;
+$a = 16807;
+$c = 0;
 */
 
-class Circle
+class Random
 {
-    private $radius;
+    const M = 2147483647;
+    const A = 16807;
+    const C = 0;
+    private $seed;
+    private $newSeed;
 
-    function __construct($radius)
+    function __construct($seed)
     {
-        $this->radius = $radius;
+        $this->seed = $seed;
+        $this->newSeed = $seed;
     }
 
-    function getArea()
+    function getNext()
     {
-        return pi() * ($this->radius ** 2);
+    
+        $this->seed = (self::A * $this->seed + self::C) % self::M;
+        return $this->seed . " ";
+      
     }
 
-    function getCircumference()
+    function reset()
     {
-        return 2 * pi() * $this->radius;
+        $this->seed = $this->newSeed;
     }
 }
 
-$circle = new Circle(3);
+$seq = new Random(100);
+echo $result1 = $seq->getNext();
+echo $result2 = $seq->getNext();
 
-print_r($circle->getArea());
+echo $result1 != $result2; // => true
+
+echo $seq->reset();
+
+echo $result21 = $seq->getNext();
+echo $result22 = $seq->getNext();
+
+echo $result1 == $result21; // => true
+echo $result2 == $result22; // => true
