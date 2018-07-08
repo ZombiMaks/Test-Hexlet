0f348b6ac6134a1766673a98161ad715b01b9d48
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-07-08 23:04:56 +0300

    stdClass

--- php/test.php
+++ php/test.php
@@ -1,51 +1,20 @@
 <?php
 /*
-Реализуйте класс PassworValidator ориентируясь на тесты.
-
-Этот валидатор поддерживает следующие опции:
-
-minLength (по-умолчанию 8) - минимальная длина пароля
-containNumbers (по-умолчанию false) - требование содержать хотя бы одну цифру
-Массив ошибок в ключах содержит название опции, а в значении текст указывающий на ошибку (тексты можно подсмотреть в тестах)
+Реализуйте функцию toStd, которая принимает на вход ассоциативнвый массив и ввозвращает объект типа stdClass такой же структуры. Выполните задачу проставляя ключи и значения вручную без использования преобразования типа.
 */
 
-class PasswordValidator
+function toStd($data)
 {
-    public $options = [
-        'minLength' => 8,
-        'containNumbers' => false
-    ];
-
-    public function __construct(array $options = [])
-    {
-        $this->options = array_merge($this->options, $options);
-    }
-
-    public function validate(string $password): array
-    {
-        $errors = [];
-        if (mb_strlen($password) < $this->options['minLength']) {
-            $errors['minLength'] = 'too small';
-        }
-
-        if ($this->options['containNumbers']) {
-            if (!$this->hasNumber($password)) {
-                $errors['containNumbers'] = 'should contain at least one number';
-            }
-        }
-
-        return $errors;
-    }
-    private function hasNumber($subject)
-    {
-        return strpbrk($subject, '1234567890') !== false;
-    }
+    //кодирует в json
+    $object = json_encode($data);
+    //из json в обьект php
+    return json_decode($object);
 }
+$data = [
+    'key' => 'value',
+    'key2' => 'value2',
+];
 
-$validator = new PasswordValidator();
-print_r($validator->validate('qwertyui'));
-print_r($validator->validate('qwerty'));
-
-$validator = new PasswordValidator(['containNumbers' => true]);print_r($validator->validate('qwertya3sdf'));
-print_r($validator->validate('qwerty'));
-print_r($validator->validate('q23ty'));
+$config = toStd($data);
+var_dump($config->key); // value
+var_dump($config->key2); // value2
