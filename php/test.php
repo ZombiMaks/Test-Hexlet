cda91abd6444961a4d031f8ddfa5744dd9c85958
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-07-10 18:40:38 +0300

    Ds

--- php/test.php
+++ php/test.php
@@ -1,20 +1,28 @@
 <?php
 /*
-Реализуйте функцию toStd, которая принимает на вход ассоциативнвый массив и ввозвращает объект типа stdClass такой же структуры. Выполните задачу проставляя ключи и значения вручную без использования преобразования типа.
+Реализуйте функцию compare, которая сравнивает две строчки набранные в редакторе. Если они равны то возвращает true, иначе - false. Особенность строчек в том они могут содержать символ #, который означает нажатие клавиши Backspace. То есть перед самим сравнением, нужно вычислить реальную строчку отображенную в редакторе.
 */
 
-function toStd($data)
+function compare(string $str1, string $str2)
 {
-    //кодирует в json
-    $object = json_encode($data);
-    //из json в обьект php
-    return json_decode($object);
-}
-$data = [
-    'key' => 'value',
-    'key2' => 'value2',
-];
+    $stack1 = new \Ds\Stack();
+    $stack2 = new \Ds\Stack();
 
-$config = toStd($data);
-var_dump($config->key); // value
-var_dump($config->key2); // value2
+    for ($i = 0; $i < strlen($str1); $i++){
+        $curr = $str1[$i];
+        if($curr !== '#'){
+            $stack1->push($curr);
+        }
+    }
+    for ($i = 0; $i < strlen($str2); $i++){
+        $curr = $str2[$i];
+        if($curr !== '#'){
+            $stack2->push($curr);
+        }
+    }
+     return sizeof($stack1) == sizeof($stack2);
+}
+
+var_dump(compare('ab#c', 'ab#c')); // true
+var_dump(compare('ab##', 'c#d#')); // true
+var_dump(compare('a#c', 'b')); // false
