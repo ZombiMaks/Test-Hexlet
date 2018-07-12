7a650a309937bfe61267dc7b1738dfeab502abe9
ZombiMaks <kolesnikmaksim8250@gmail.com>
2018-07-12 06:51:00 +0300

    collection

--- php/test.php
+++ php/test.php
@@ -1,28 +1,36 @@
 <?php
 /*
-Реализуйте функцию compare, которая сравнивает две строчки набранные в редакторе. Если они равны то возвращает true, иначе - false. Особенность строчек в том они могут содержать символ #, который означает нажатие клавиши Backspace. То есть перед самим сравнением, нужно вычислить реальную строчку отображенную в редакторе.
+Реализуйте класс DeckOfCards который описывает собой колоду карт и умеет ее мешать.
+
+Конструктор класса принимает на вход массив в котором перечислены номиналы карт в единственном экземпляре, например, [6, 7, 8, 9, 10, 'king']. Затем, с помощью, метода getShuffled можно получить полную колоду (то есть такую колоду, в которой каждая карта встречается 4 раза. Для простоты не учитываем масть) в виде массива отсортированную случайным образом
 */
 
-function compare(string $str1, string $str2)
+class DeckOfCards
 {
-    $stack1 = new \Ds\Stack();
-    $stack2 = new \Ds\Stack();
+    private $carts = array();
+    private $cart;
 
-    for ($i = 0; $i < strlen($str1); $i++){
-        $curr = $str1[$i];
-        if($curr !== '#'){
-            $stack1->push($curr);
-        }
+    public function __construct($carts){
+        $colection = [];
+       foreach($carts as $value){
+           $colection[] = $value;
+           $colection[] = $value;
+           $colection[] = $value;
+           $colection[] = $value;
+       }
+        $this->cart =  $colection;
+        $this->carts = $carts;
     }
-    for ($i = 0; $i < strlen($str2); $i++){
-        $curr = $str2[$i];
-        if($curr !== '#'){
-            $stack2->push($curr);
-        }
+
+    public function getShuffled()
+    {
+       $colection = $this->cart;
+       shuffle($colection);
+       return $colection;  
     }
-     return sizeof($stack1) == sizeof($stack2);
 }
 
-var_dump(compare('ab#c', 'ab#c')); // true
-var_dump(compare('ab##', 'c#d#')); // true
-var_dump(compare('a#c', 'b')); // false
+
+$deck = new DeckOfCards([2, 3, 5]);
+print_r($deck->getShuffled()); // [2, 3, 3, 3, 2, 3, 2, 2]
+print_r($deck->getShuffled()); // [3, 3, 2, 2, 2, 3, 3, 2]
